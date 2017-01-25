8272555c14dbbc998c65b8361c6a2c6ef616d2ce
lucas.alves <lucasfealves@icloud.com>
2017-01-25 15:13:31 -0200

    Commit 4

--- System/ProjetoPesquisa/views.py
+++ System/ProjetoPesquisa/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Categorias, Produtos
 from .forms import CategoriaForm, ProdutosForm
 #import pymsgbox
-from tkinter import messagebox
-from asyncio.tasks import sleep
-import tkinter
-
-top = tkinter.Tk()
-top.wm_withdraw()
+#from tkinter import messagebox
+#import tkinter
 
 # Create your views here.
 def home(request):
@@ -38,11 +34,11 @@ def Categoria_edit(request, pk):
     return render(request, 'ProjetoPesquisa/categoria_edit.html', {'form': form})    
 def Categoria_delete(request, pk):
     categoria = get_object_or_404(Categorias, pk=pk)
+    categoria.delete()
     # response = pymsgbox.confirm(text='Deseja realmente deletar este registro', title='Alerta de Sistema', buttons=['OK', 'Cancel'])
-    response = messagebox.askyesno('Alerta de Sistema','Deseja realmente deletar este registro?')
+    #response = messagebox.askyesno('Alerta de Sistema','Deseja realmente deletar este registro?')
     #sleep(1000)
-    if response:
-        categoria.delete();
+    #if response:
     return redirect('Categoria')
 def Produto(request):
     produtos = Produtos.objects.order_by('nome')
@@ -72,5 +68,3 @@ def Produtos_delete(request, pk):
     produtos = get_object_or_404(Produtos, pk=pk)
     produtos.delete();
     return redirect('Produtos')
-
-top.mainloop()
